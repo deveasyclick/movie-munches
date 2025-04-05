a23b10fe8a5a50a7c5efc9089947d5f574f1ea46
Yusuf <easyclick06@gmail.com>
2025-04-05 16:38:59 +0100

    * Update movie card to display more information about the movies

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -3,16 +3,38 @@ import type { Movie } from "../types/Movie";
 interface MovieCardProps {
   movie: Movie;
 }
-const MovieCard = ({ movie: { title, id, poster_path } }: MovieCardProps) => {
+const MovieCard = ({
+  movie: {
+    title,
+    id,
+    poster_path,
+    vote_average,
+    original_language,
+    release_date,
+  },
+}: MovieCardProps) => {
   const posterPath = poster_path
     ? `https://image.tmdb.org/t/p/w500${poster_path}`
-    : "https://via.placeholder.com/500x750?text=No+Image+Available";
+    : "/no-movie.png";
   return (
-    <div className="movie-card">
+    <div className="movie-card" key={id}>
       <img src={posterPath} alt={title} />
-      <h3 className="text-white" key={id}>
-        {title}
-      </h3>
+      <div className="mt-4">
+        <h3>{title}</h3>
+      </div>
+
+      <div className="content">
+        <div className="rating">
+          <img src="star.svg" alt="Star Icon" />
+          <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
+        </div>
+        <span>•</span>
+        <div className="lang">{original_language}</div>
+        <span>•</span>
+        <div className="year">
+          {release_date ? release_date.split("-")[0] : "N/A"}
+        </div>
+      </div>
     </div>
   );
 };
